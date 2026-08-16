32105465a8709c775eaf7cc77fe7e0a54f6c4554
MinhMussic <minh123tho@gmail.com>
2026-08-16 22:33:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Hethongtrungtam/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
